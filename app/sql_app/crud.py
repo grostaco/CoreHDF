43041aa243e6b0ca17029bdfed439eb939643b06
grostaco <grostaco@gmail.com>
2021-03-24 18:24:41 +0700

    fix(app): circular import

--- app/sql_app/crud.py
+++ app/sql_app/crud.py
@@ -1,11 +1,16 @@
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from . import models, schemas
-from ..dependencies import pwd_context
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
 
 
 def get_user_by_email(db: Session, email: str):
@@ -17,7 +22,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(email=user.email, username=user.username, hashed_password=pwd_context.hash(user.password))
+    db_user = models.User(email=user.email, username=user.username, full_name=user.full_name, hashed_password=pwd_context.hash(user.password))
 
     db.add(db_user)
     db.commit()
