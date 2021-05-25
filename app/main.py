7c544d3c7c3ffe88ae428ee1ecc823f5b90c8fd1
BuildTools <unconfigured@null.spigotmc.org>
2021-05-25 21:36:49 +0700

    refactor(app): app location changed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from .routers import users, token
+from .routers import users, token, query
 
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(token.router)
+app.include_router(query.router)
--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import uvicorn
-from fastapi import FastAPI
-from app.routers import token, query, users
+import app.main as main
 
-app = FastAPI()
-app.include_router(token.router)
-app.include_router(query.router)
-app.include_router(users.router)
-
-uvicorn.run(app)
+uvicorn.run(main.app)
