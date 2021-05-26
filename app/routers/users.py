6ebb2697da520a74c7dc376c180bd58057ebf9c4
BuildTools <unconfigured@null.spigotmc.org>
2021-05-26 11:37:44 +0700

    refactor(app): removed redundant route /me/items

--- app/routers/users.py
+++ app/routers/users.py
@@ -27,7 +27,3 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
                             email=current_user.email)
     return current_user
 
-
-@router.get("/me/items/")
-async def read_own_items(current_user: UserBase = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
